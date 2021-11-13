9772fa248ca46a4be1bef55d86b2518ce1985938
Villicus-np <s10207947@connect.np.edu.sg>
2021-11-13 10:45:27 +0800

    Add project files.

---
+++ OCBC Online Joint Account/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OCBC_Online_Joint_Account.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
